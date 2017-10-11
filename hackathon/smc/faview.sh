9faa27aa23fe41c8963e5c027346abb1b26e1a02
latha.krishnaswamy <latha.krishnaswamy@oracle.com>
2017-10-11 14:49:42 -0700

    wercker build time!

--- hackathon/smc/faview.sh
+++ hackathon/smc/faview.sh
@@ -1,3 +1,38 @@
 # Create view
 
 ade createview FUSIONAPPS -series FUSIONAPPS_PT.V2MIB_LINUX.X64
+ade useview FUSIONAPPS
+cd $ADE_VIEW_ROOT/fusionapps/dist/fusionapps/fa/deploy
+ade expand *
+ade mkrpivate *
+ade mkprivate */*
+ade mkprivate */*/*
+ade mkprivate */*/*/*
+ade mkprivate */*/*/*/*
+find . -nMW ".ADE_PATH" -EXEC RM {} \;
+
+CHMOD -R 755 *
+tar -cvhzf fa_deploy.tar.gz *.ear *.war
+
+# deploying the ear to your SMC environment 
+
+## shutdown all servers and make sure they are all down before working on the next set of tasks 
+## ssh to the env
+
+ssh lakrishn@rws66550fwks.us.oracle.com
+
+cd /u01/APPLTOP/fusionapps/applications/fa
+
+rm -r /u01/APPLTOP/instance/lcm/finops/backup_ear/deploy.backup
+
+cp -R deploy /u01/APPLTOP/instance/lcm/finops/backup_ear/deploy.backup
+
+cd deploy
+
+scp lakrishn@rws66550fwks.us.oracle.com:/scratch/lakrishn/view_storage/lakrishn_FUSIONAPPS/fusionapps/dist/fusionapps/fa/deploy/fa_deploy.tar.gz .
+
+tar -xzvf fa_deploy.tar.gz
+
+# start all servers
+
+
